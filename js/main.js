e2b2422e12373acee700d29d6e9698bd06ea4d7c
Ever <ever4ever@yandex.ru>
2021-11-26 15:41:56 +0300

    Реализована работа меню, появление модального окна и работа табов

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 window.addEventListener("DOMContentLoaded", () => {
   "use strict";
   // Timer
-  function countTimer(deadline) {
+  const countTimer = (deadline) => {
     const timerHours = document.querySelector("#timer-hours"),
       timerMinutes = document.querySelector("#timer-minutes"),
       timerSeconds = document.querySelector("#timer-seconds");
 
-    function validTimeSlot(timeSlot) {
+    const validTimeSlot = (timeSlot) => {
       if (timeSlot < 0) {
         timeSlot = 0;
       }
       return timeSlot;
-    }
+    };
 
-    function getTimeRemaining() {
+    const getTimeRemaining = () => {
       const dateStop = new Date(deadline).getTime(),
         dateNow = new Date().getTime(),
         timeRemaining = validTimeSlot((dateStop - dateNow) / 1000),
@@ -21,29 +21,104 @@ window.addEventListener("DOMContentLoaded", () => {
         minutes = Math.floor((timeRemaining / 60) % 60),
         hours = Math.floor(timeRemaining / 60 / 60);
       return { timeRemaining, hours, minutes, seconds };
-    }
+    };
 
-    function standardize(timeValue) {
-      if (timeValue.toString().length === 1) {
-        timeValue = `0${timeValue}`;
+    const addZero = (num) => {
+      if (num <= 9) {
+        num = `0${num}`;
       }
-      return timeValue;
-    }
+      return num;
+    };
 
-    function updateClock() {
+    const updateClock = () => {
       const timer = getTimeRemaining();
 
-      timerHours.textContent = standardize(timer.hours);
-      timerMinutes.textContent = standardize(timer.minutes);
-      timerSeconds.textContent = standardize(timer.seconds);
+      timerHours.textContent = addZero(timer.hours);
+      timerMinutes.textContent = addZero(timer.minutes);
+      timerSeconds.textContent = addZero(timer.seconds);
 
       if (timer.timeRemaining <= 0) {
         clearInterval(idInterval);
       }
-    }
+    };
 
     updateClock();
-  }
+  };
 
   const idInterval = setInterval(countTimer, 1000, "26 november 2021");
+
+  // Menu
+  const toggleMenu = () => {
+    const btnMenu = document.querySelector(".menu"),
+      menu = document.querySelector("menu"),
+      closeBtn = document.querySelector(".close-btn"),
+      menuItems = menu.querySelectorAll("ul>li");
+
+    const handlerMenu = () => {
+      menu.classList.toggle("active-menu");
+    };
+
+    btnMenu.addEventListener("click", handlerMenu);
+    closeBtn.addEventListener("click", handlerMenu);
+
+    menuItems.forEach((elem) => elem.addEventListener("click", handlerMenu));
+  };
+
+  toggleMenu();
+
+  // Popup
+  const togglePopUp = () => {
+    const popup = document.querySelector(".popup"),
+      popupBtns = document.querySelectorAll(".popup-btn"),
+      popupCloseBtn = document.querySelector(".popup-close");
+
+    popupBtns.forEach((elem) =>
+      elem.addEventListener("click", () => {
+        popup.style.display = "block";
+      })
+    );
+
+    popupCloseBtn.addEventListener(
+      "click",
+      () => (popup.style.display = "none")
+    );
+  };
+
+  togglePopUp();
+
+  // Tabs
+  const tabs = () => {
+    const tabHeader = document.querySelector(".service-header"),
+      tab = tabHeader.querySelectorAll(".service-header-tab"),
+      tabContent = document.querySelectorAll(".service-tab");
+
+    const toggleTabContent = (index) => {
+      for (let i = 0; i < tabContent.length; i++) {
+        if (index === i) {
+          tab[i].classList.add("active");
+          tabContent[i].classList.remove("d-none");
+        } else {
+          tab[i].classList.remove("active");
+          tabContent[i].classList.add("d-none");
+        }
+      }
+    };
+
+    tabHeader.addEventListener("click", (event) => {
+      let target = event.target;
+      while (target !== tabHeader) {
+        if (target.classList.contains("service-header-tab")) {
+          tab.forEach((item, i) => {
+            if (item === target) {
+              toggleTabContent(i);
+            }
+          });
+          return;
+        }
+        target = target.parentNode;
+      }
+    });
+  };
+
+  tabs();
 });
